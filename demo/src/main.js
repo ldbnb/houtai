06b91f2d63a660e594c1a2c49fa02c146f3ca0e1
bqb <wy1985591589@126.com>
2022-04-25 08:59:12 +0800

    新增login页面

---
+++ demo/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import '@/assets/css/public.css'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
+import 'normalize.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
---
+++ demo/src/plugins/element.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input } from 'element-ui'
+Vue.use(Button).use(Form).use(FormItem).use(Input)
